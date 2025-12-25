5fee777650c883e03385f4f7b671df35dbdf56f3
Dharshini Tamizhmaran <dharshutamizh@gmail.com>
2025-12-25 22:37:21 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -1,22 +1,15 @@
 const express = require("express");
 const cors = require("cors");
-
 const dashboardRoute = require("./routes/dashboard");
 const forecastRoute = require("./routes/forecast");
 const whatIfRoute = require("./routes/whatif");
-
 const app = express();
-
 app.use(cors());
 app.use(express.json());
-
 app.use("/dashboard", dashboardRoute);
 app.use("/forecast", forecastRoute);
 app.use("/whatif", whatIfRoute);
-
-
 const PORT = process.env.PORT || 5000;
-
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
